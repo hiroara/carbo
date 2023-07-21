eb1992e6b9c61b99160b2a14fe10746367a3d55b
Arai Hiroki <hiroara62@gmail.com>
2023-07-22 00:43:17 +0900

    Add pipe.MapWithCache

--- pipe/map.go
+++ pipe/map.go
@@ -3,34 +3,52 @@ package pipe
 import (
 	"context"
 
+	"github.com/hiroara/carbo/cache"
 	"github.com/hiroara/carbo/task"
 )
 
 type MapOp[S, T any] struct {
-	run PipeFn[S, T]
+	run MapFn[S, T]
 }
 
 type MapFn[S, T any] func(context.Context, S) (T, error)
 
 func Map[S, T any](fn MapFn[S, T]) *MapOp[S, T] {
 	return &MapOp[S, T]{
-		run: func(ctx context.Context, in <-chan S, out chan<- T) error {
-			for i := range in {
-				mapped, err := fn(ctx, i)
-				if err != nil {
-					return err
-				}
-				out <- mapped
-			}
-			return nil
-		},
+		run: fn,
 	}
 }
 
 func (op *MapOp[S, T]) AsTask() task.Task[S, T] {
-	return task.Task[S, T](FromFn(op.run))
+	return task.Task[S, T](FromFn(op.pipeFn()))
 }
 
 func (op *MapOp[S, T]) Concurrent(concurrency int) *Pipe[S, T] {
-	return ConcurrentFromFn(op.run, concurrency)
+	return ConcurrentFromFn(op.pipeFn(), concurrency)
+}
+
+func (op *MapOp[S, T]) pipeFn() PipeFn[S, T] {
+	return func(ctx context.Context, in <-chan S, out chan<- T) error {
+		for i := range in {
+			mapped, err := op.run(ctx, i)
+			if err != nil {
+				return err
+			}
+			out <- mapped
+		}
+		return nil
+	}
+}
+
+func MapWithCache[S, T, K, V any](fn MapFn[S, T], sp cache.Spec[S, T, K, V]) *MapOp[S, T] {
+	return Map(func(ctx context.Context, el S) (T, error) {
+		var zero T
+
+		ent, err := cache.GetEntry(sp, el)
+		if err != nil {
+			return zero, err
+		}
+
+		return ent.Run(ctx, cache.CacheableFn[S, T](fn))
+	})
 }
--- pipe/map_test.go
+++ pipe/map_test.go
@@ -8,7 +8,9 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/hiroara/carbo/cache"
 	"github.com/hiroara/carbo/flow"
+	"github.com/hiroara/carbo/messaging/marshal"
 	"github.com/hiroara/carbo/pipe"
 	"github.com/hiroara/carbo/sink"
 	"github.com/hiroara/carbo/source"
@@ -18,11 +20,12 @@ import (
 func TestMap(t *testing.T) {
 	t.Parallel()
 
-	src := source.FromSlice([]string{"item1", "item2"})
+	src := source.FromSlice([]string{"item1", "item2", "item2"})
 
-	m := pipe.Map(func(ctx context.Context, s string) (string, error) {
+	fn := func(ctx context.Context, s string) (string, error) {
 		return s + s, nil
-	})
+	}
+	m := pipe.Map(fn)
 
 	runFlowWithMap := func(mappingTask task.Task[string, string]) ([]string, error) {
 		out := make([]string, 0)
@@ -55,7 +58,7 @@ func TestMap(t *testing.T) {
 		out, err := runFlowWithMap(m.AsTask())
 		require.NoError(t, err)
 
-		assert.Equal(t, []string{"item1item1", "item2item2"}, out)
+		assert.Equal(t, []string{"item1item1", "item2item2", "item2item2"}, out)
 	})
 
 	t.Run("Concurrent", func(t *testing.T) {
@@ -64,6 +67,35 @@ func TestMap(t *testing.T) {
 		out, err := runFlowWithMap(m.Concurrent(2).AsTask())
 		require.NoError(t, err)
 
-		assert.ElementsMatch(t, []string{"item1item1", "item2item2"}, out)
+		assert.ElementsMatch(t, []string{"item1item1", "item2item2", "item2item2"}, out)
+	})
+
+	t.Run("Cache", func(t *testing.T) {
+		t.Parallel()
+
+		ms := marshal.Gob[string]()
+		cs := cache.NewMemoryStore[[]byte]()
+		sp := cache.NewMarshalSpec[string, string, string](
+			cs,
+			func(el string) (string, error) {
+				return el, nil
+			},
+			ms,
+		)
+
+		out, err := runFlowWithMap(pipe.MapWithCache(fn, sp).AsTask())
+		require.NoError(t, err)
+
+		assert.ElementsMatch(t, []string{"item1item1", "item2item2", "item2item2"}, out)
+
+		ctx := context.Background()
+		bs, ok, err := cs.Get(ctx, "item1")
+		require.NoError(t, err)
+
+		if assert.True(t, ok) {
+			v, err := ms.Unmarshal(bs)
+			require.NoError(t, err)
+			assert.Equal(t, "item1item1", v)
+		}
 	})
 }
