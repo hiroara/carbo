5594c7916bab8c7dca976fcc9d692b75c021093a
Arai Hiroki <hiroara62@gmail.com>
2023-07-25 09:34:15 +0900

    Add docs for deferrer package

--- deferrer/deferrer.go
+++ deferrer/deferrer.go
@@ -1,15 +1,18 @@
 package deferrer
 
+// An embeddable struct that can be used to implement Defer on a struct.
 type Deferrer struct {
 	deferred []func()
 }
 
+// Run all registered deferred functions.
 func (d *Deferrer) RunDeferred() {
 	for _, fn := range d.deferred {
 		fn()
 	}
 }
 
+// Register a function to defer the execution.
 func (d *Deferrer) Defer(fn func()) {
 	d.deferred = append(d.deferred, fn)
 }
---
+++ deferrer/doc.go
@@ -0,0 +1,3 @@
+// Package provides an struct-embeddable implementation of Defer
+// that is required by task.Task interface.
+package deferrer
