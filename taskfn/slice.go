43a5dbc8c51cd75a6e14480f258bae87cc0398fd
Arai Hiroki <hiroara62@gmail.com>
2023-07-29 15:28:00 +0900

    feat: add taskfn.SourceToSlice and taskfn.SliceToSink

--- taskfn/slice.go
+++ taskfn/slice.go
@@ -9,7 +9,7 @@ import (
 	"github.com/hiroara/carbo/task"
 )
 
-// Function that takes an input slice and returns an output slice.
+// Function that takes an input slice and returns an output slice or an error.
 type SliceToSliceFn[S, T any] func(context.Context, []S) ([]T, error)
 
 // Convert a task into SliceToSliceFn.
@@ -34,5 +34,45 @@ func SliceToSlice[S, T any](t task.Task[S, T]) SliceToSliceFn[S, T] {
 			return nil, err
 		}
 		return outputs, nil
+	}
+}
+
+// Function that does not take any arguments and returns an output slice or an error.
+type ToSliceFn[T any] func(context.Context) ([]T, error)
+
+// Convert a source task into ToSliceFn.
+// This is useful in case that a task does not take any inputs and
+// returns a few length of outputs, for example, for testing a source task.
+func SourceToSlice[T any](src source.Source[T]) ToSliceFn[T] {
+	return func(ctx context.Context) ([]T, error) {
+		outputs := make([]T, 0)
+		out := task.Connect(
+			src.AsTask(),
+			sink.ToSlice(&outputs).AsTask(),
+			0,
+		)
+
+		if err := flow.FromTask(out).Run(ctx); err != nil {
+			return nil, err
+		}
+		return outputs, nil
+	}
+}
+
+// Function that takes an input slice and returns nothing or an error.
+type SliceFn[S any] func(context.Context, []S) error
+
+// Convert a sink task into SliceFn.
+// This is useful in case that a task takes a few length of inputs and
+// returns nothing as outputs, for example, for testing a sink task.
+func SliceToSink[S any](sin sink.Sink[S]) SliceFn[S] {
+	return func(ctx context.Context, inputs []S) error {
+		out := task.Connect(
+			source.FromSlice(inputs).AsTask(),
+			sin.AsTask(),
+			0,
+		)
+
+		return flow.FromTask(out).Run(ctx)
 	}
 }
--- taskfn/slice_test.go
+++ taskfn/slice_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/hiroara/carbo/pipe"
+	"github.com/hiroara/carbo/sink"
+	"github.com/hiroara/carbo/source"
 	"github.com/hiroara/carbo/taskfn"
 )
 
@@ -22,4 +24,27 @@ func TestSliceToSlice(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, []string{"item1item1", "item2item2"}, out)
+}
+
+func TestSourceToSlice(t *testing.T) {
+	t.Parallel()
+
+	fn := taskfn.SourceToSlice(source.FromSlice([]string{"item1", "item2"}).AsSource())
+
+	out, err := fn(context.Background())
+	require.NoError(t, err)
+
+	assert.Equal(t, []string{"item1", "item2"}, out)
+}
+
+func TestSliceToSink(t *testing.T) {
+	t.Parallel()
+
+	out := make([]string, 0)
+	fn := taskfn.SliceToSink(sink.ToSlice(&out).AsSink())
+
+	err := fn(context.Background(), []string{"item1", "item2"})
+	require.NoError(t, err)
+
+	assert.Equal(t, []string{"item1", "item2"}, out)
 }
